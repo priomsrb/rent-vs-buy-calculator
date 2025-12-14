d89a9e280e46828d31cd1f2e837847392b752d69
Shafqat Bhuiyan <priomsrb@gmail.com>
2025-12-15 09:20:03 +1100

    Add posthog

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { PostHogProvider } from "posthog-js/react";
 import "./index.css";
 
 import { routeTree } from "./routeTree.gen";
@@ -15,6 +16,16 @@ declare module "@tanstack/react-router" {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={{
+        api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+        defaults: "2025-05-24",
+        capture_exceptions: true, // This enables capturing exceptions using Error Tracking
+        debug: import.meta.env.MODE === "development",
+      }}
+    >
+      <RouterProvider router={router} />
+    </PostHogProvider>
   </StrictMode>,
 );
